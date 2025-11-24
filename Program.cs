4f5441fe298e15167bc9ceeeebb78857022378c8
henrihunt4167 <henriiii0909@gmail.com>
2025-11-24 11:43:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace castimine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double komakohagaarv = 9.45d;
+            Console.WriteLine((int)komakohagaarv);
+            int roundedarv = (int)Math.Round(komakohagaarv);
+            Console.WriteLine(roundedarv);
+
+
+            int arv = 35;
+            string arvtekstina = arv.ToString();
+            Console.WriteLine(arvtekstina);
+            Console.WriteLine(arv);
+            double arvdouble = 279346559d;
+            int  arvint = 0;
+            arvint = (int)arvdouble;
+            double uusdouble = arvint;
+        }
+        //kirjuta programm mis küsib kasutajalt meetrites tema pikkust (komakohtadega)
+        //teisenda kasutaja pikkus sentimeetriteks
+        //küsimine esialgu on tekstikujul (kasutaja sisestab teksti),
+        //programm ise peab tekstist välja lugema komakohaga arvu
+    }
+}
+string pikkusm = "";
+Console.WriteLine("Sisesta oma pikkus");
+pikkusm = Console.ReadLine();
+float pikkusFm = float.Parse(pikkusm);
+int pikkuscm = (int)pikkusFm * 100;
+Console.WriteLine($"Sinu pikkus sentimeetrites on: {pikkuscm}");
+
